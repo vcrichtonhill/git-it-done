48d2db615e6be81856c3cf21c8d17f91008b1b65
Vee Crichton-Hill <vcrichtonhill@gmail.com>
2022-04-08 21:22:17 -0500

    repo issue list

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,25 +1,39 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if(repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo) {
     console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-
+    
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
-
+                
                 // check if api has paginated issues
                 if (response.headers.get("Link")) {
                     displayWarning(repo);
                 }
             });
         } else {
-            alert("There was a problem with your request!")
+            document.location.replace("./index.html");
         }
     });
+    getRepoName();
 };
 
 var displayIssues = function(issues) {
@@ -70,5 +84,5 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-
-getRepoIssues("vcrichtonhill/git-it-done");
+getRepoName();
+getRepoIssues();
